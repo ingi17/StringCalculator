4ca84d72828b6c75b6f8b7b6d7db5c681a96ad58
Ingi <ingi17@ru.is>
2018-10-13 14:46:17 +0000

    Test cases for undef. number of arguments

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -9,18 +9,9 @@ test('1 should return 1', () => {
 test('1, 2 should return 3', () => {
   expect(calc("1,2")).toBe(3);
 });
-test('1, 2, 3, 4 should return 3', () => {
-  expect(calc("1,2,3,4")).toBe(3);
+test('1, 2, 3, 4 should return 10', () => {
+  expect(calc("1,2,3,4")).toBe(10);
 });
-
-
-/*
-test("", 0);
-test("1", 1);
-test("1,2", 3);
-test("1,2,3,4", 10);
-
-function test(i, o) {
-	expect(calc.add(i).toBe(o));
-}
-*/
+test('2, 0, 0, 0, 5, 8 should return 15', () => {
+  expect(calc("2,0,0,0,5,8")).toBe(15);
+});
