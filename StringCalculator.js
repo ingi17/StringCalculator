ad4cf446d86aa0391c093021f42e6be699640e6f
Ingi <ingi17@ru.is>
2018-10-13 13:56:17 +0000

    StringCalculator.Add() working for all test cases

---
+++ StringCalculator.js
@@ -0,0 +1,13 @@
+function add(numberStr) {
+	if (!numberStr) return 0; // Skip everything and return 0 if String is empty.
+
+	var splitStr = numberStr.split(','),
+		sum = 0;
+	
+	for (i = 0; i < splitStr.length; i++) {
+    	if (i >= 2) return sum; // Stop adding and return if input has more than 2 numbers.
+		sum += parseInt(splitStr[i]);
+	}
+	return sum;
+}
+module.exports = add;
