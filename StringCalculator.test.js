ef277f1ec620a889bd65d1f90a4981283476e5a8
Ingi <ingi17@ru.is>
2018-10-13 13:13:30 +0000

    Empty string test case for add() working

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,8 +1,18 @@
 const calc = require('./StringCalculator');
 
 test('Empty string should return 0', () => {
-  expect(calc.add("")).toBe(0);
+  expect(calc("")).toBe(0);
 });
+test('1 should return 1', () => {
+  expect(calc("1")).toBe(1);
+});
+test('1, 2 should return 3', () => {
+  expect(calc("1,2")).toBe(3);
+});
+test('1, 2, 3, 4 should return 3', () => {
+  expect(calc("1,2,3,4")).toBe(3);
+});
+
 
 /*
 test("", 0);
